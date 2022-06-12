6c454f37a5f2e8133009cab33cc5f07fd8c7d4ad
junior batista <jr3077932@gmail.com>
2022-06-11 22:57:37 -0300

    Adc reserva

---
+++ site/src/api/reservaApi.js
@@ -0,0 +1,16 @@
+import axios  from "axios";
+const api = axios.create({
+    baseURL: 'http://localhost:5000'
+})
+
+
+export async function adicionaReserva(mesa, cliente, pessoas, reservas, observacao){
+    const resposta = await api.post ('./reserva', {
+        mesa:mesa,
+        cliente:cliente,
+        pessoas:pessoas,
+        reservas:reservas,
+        observacao:observacao
+    })
+    return resposta.data;
+}
--- site/src/pages/adicionar mesas/index.js
+++ site/src/pages/adicionar mesas/index.js
@@ -1,7 +1,26 @@
 import './index.scss';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+import { adicionaReserva } from '../../api/reservaApi';
 
 export default function Index(){
+        const [mesa, setMesa] = useState(0);
+        const [cliente, setCliente] = useState('');
+        const [pessoas, setPessoas] = useState(0);
+        const [reservas, setReservas] = useState('');
+        const [observacao, setObservacao] = useState('');
+
+        async function finalizarClick() {
+            try{
+                const resposta = await adicionaReserva(mesa, cliente, pessoas, reservas, observacao);
+                alert('reserva cadastrada com sucesso');
+
+            }catch(err){
+                alert(err.message);
+            }
+        }
+        
     return(
         <div className='adicionar'>
             <section className="fx-reserva">
@@ -16,34 +35,34 @@ export default function Index(){
                         <div className="cima">
                             <div className="column">
                                 <div className="texto , font">Mesa</div>
-                                <input className="input1" type="text"/>
+                                <input className="input1" type="number" value={mesa} onChange={e => setMesa(e.target.value)}/>
                             </div>
                             <div className="column">
                                 <div className="texto , font">Nome</div>
-                                <input className="input2" type="text"/>
+                                <input className="input2" type="text" value={cliente} onChange={e => setCliente(e.target.value)}/>
                             </div>    
                         </div>
                         <div className='baixo'>
                             <div className="">
                                 <div className="texto , font">Qtd de pessoas</div>
-                                <input className="input1" type="text"/>
+                                <input className="input1" type="number" value={pessoas} onChange={ e => setPessoas(e.target.value)}/>
                             </div>
                             <div className=''>
                                 <div className="texto , font">Data e hora</div>
-                                <input type="date"className="input3"/>
+                                <input type="date"className="input3" value={reservasPPP} onChange={ e => setReservas(e.target.value)}/>
                             </div>
                         </div>
                         <div className='baixo-2'>
                             <div>
                                 <div className='texto , font'>Observações</div>
-                                <textarea type="text" className='input-ob'/>
+                                <textarea type="text" className='input-ob'value={observacao} onChange={ e => setObservacao(e.target.value)}/>
                             </div>
                             <img style={{"marginTop":"20px"}}width="310px" height="200px"src='/images/adicionar mesa.png'alt=''/> 
                         </div>
                         
                        
                         <div className="margin-botao">
-                            <Link to="/Consultar" className="botao , font">Finalizar</Link>
+                            <Link to="/Consultar" className="botao , font" onClick={finalizarClick} >Finalizar</Link>
                         </div>
                     </div>
                 </div>
